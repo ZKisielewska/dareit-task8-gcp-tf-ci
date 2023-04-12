1d3f402acf436c204ca608b7569cef1b91248593
ZKisielewska <kisielewskazpl@gmail.com>
2023-04-12 21:25:07 +0200

    feat: creat website and CI/CD workflow

---
+++ main.tf
@@ -0,0 +1,55 @@
+resource "google_compute_instance" "dareit-vm-ci" {
+  name         = "dareit-task8-gc-vm-tf"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  tags = ["dareit"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        managed_by_terraform = "true"
+      }
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+}
+
+resource "google_storage_bucket" "static-website" {
+  name          = "dareit-task8-gc-ws-tf"
+  location      = "US"
+  force_destroy = false
+
+  uniform_bucket_level_access = false
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+  cors {
+    origin          = ["*"]
+    method          = ["*"]
+    response_header = ["*"]
+    max_age_seconds = 3600
+  }
+}
+
+resource "google_storage_bucket_object" "static-website" {
+  name   = "index.html"
+  source = "website/index.html"
+  bucket = google_storage_bucket.static-website.name
+}
+
+resource "google_storage_bucket_object" "file" {
+  name   = "solarsystem.jpg"
+  source = "website/solarsystem.jpg"
+  bucket = google_storage_bucket.static-website.name
+}
